6cd319ee1578af1614d87289bb0e6b182ddac295
Korhan Aladağ <50011233+KorhanAladag@users.noreply.github.com>
2023-10-04 13:17:33 +0300

    Add files via upload

---
+++ Quizzical/components/Background.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function Background(){
+    return (
+        <div className="bg-container">
+            <img src="images/blob-botl.png" className="bot-left" />  
+            <img src="images/blob-topr.png" className="top-right" />
+        </div>    
+    )
+}
---
+++ Quizzical/components/LoadingPage.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default function LoadingPage(){
+    return (
+    <div className="loadingpage-container">
+        <div className="spinner"></div>
+        <p>Loading quiz data...</p>
+    </div>
+    )
+    
+}
